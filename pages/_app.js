f0ea766767e4a705ddb51f1b2123989b54bd1a97
LHaitam <77359501+LHaitam@users.noreply.github.com>
2024-10-29 19:25:13 +0100

    Update _app.js (add GTM tag)
    <!-- Google Tag Manager -->
    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
    })(window,document,'script','dataLayer','GTM-K5GCBDD8');</script>
    <!-- End Google Tag Manager -->

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import Script from 'next/script';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/globals.css';
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Google Tag Manager */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K5GCBDD8');
+          `,
+        }}
+      />
+      {/* End Google Tag Manager */}
       <Navbar />
       <Component {...pageProps} />
       <Footer />
